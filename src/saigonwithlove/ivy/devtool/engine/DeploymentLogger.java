c7d41d05d1597e2a07d6e3106a6a0bdd5a1c955a
Cavoi Rom <cavoirom.wanted@yahoo.com>
2020-03-25 10:09:48 +0700

    Improve PMV reload command, work identically with official deployment

---
+++ src/saigonwithlove/ivy/devtool/engine/DeploymentLogger.java
@@ -0,0 +1,42 @@
+package saigonwithlove.ivy.devtool.engine;
+
+import ch.ivyteam.ivy.deployment.IDeploymentLogger;
+import ch.ivyteam.log.ILogger;
+
+public class DeploymentLogger implements IDeploymentLogger {
+  private ILogger logger;
+
+  public DeploymentLogger(ILogger logger) {
+    this.logger = logger;
+  }
+
+  @Override
+  public void info(String message) {
+    logger.info(message);
+  }
+
+  @Override
+  public void info(String message, Throwable throwable) {
+    logger.info(message, throwable);
+  }
+
+  @Override
+  public void warning(String message) {
+    logger.warn(message);
+  }
+
+  @Override
+  public void warning(String message, Throwable throwable) {
+    logger.warn(message, throwable);
+  }
+
+  @Override
+  public void error(String message) {
+    logger.error(message);
+  }
+
+  @Override
+  public void error(String message, Throwable throwable) {
+    logger.error(message, throwable);
+  }
+}
--- src/saigonwithlove/ivy/devtool/engine/ProcessModelVersions.java
+++ src/saigonwithlove/ivy/devtool/engine/ProcessModelVersions.java
@@ -1,12 +1,19 @@
 package saigonwithlove.ivy.devtool.engine;
 
 import ch.ivyteam.ivy.application.ActivityOperationState;
-import ch.ivyteam.ivy.application.ILibrary;
+import ch.ivyteam.ivy.application.IProcessModel;
 import ch.ivyteam.ivy.application.IProcessModelVersion;
+import ch.ivyteam.ivy.deployment.restricted.DeploymentManagerFactory;
 import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.java.JavaConfigurationNavigationUtil;
+import ch.ivyteam.util.concurrent.Poll;
 import com.google.common.base.Preconditions;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.codehaus.plexus.util.DirectoryScanner;
+import org.eclipse.core.runtime.NullProgressMonitor;
 
 public class ProcessModelVersions {
 
@@ -20,45 +27,54 @@ public class ProcessModelVersions {
   }
 
   private static void reload(IProcessModelVersion pmv) {
-    deactivate(pmv);
-    JavaConfigurationNavigationUtil.getJavaConfiguration(pmv)
-        .getClassLoaderHolder()
-        .updateRepositories();
-    activate(pmv);
-    pmv.getLibrary().getAllDependentLibraries().stream()
-        .map(ILibrary::getProcessModelVersion)
-        .forEach(ProcessModelVersions::reload);
-  }
-
-  private static void activate(IProcessModelVersion pmv) {
-    pmv.activate();
-    int timeOut = 10;
-    while (pmv.getActivityOperationState() != ActivityOperationState.ACTIVE) {
-      if (timeOut < 0) {
-        throw new RuntimeException("Process Model Version takes to long to activate.");
-      }
-      sleep();
-      timeOut -= 1;
+    try {
+      deactivate(pmv);
+      DeploymentManagerFactory.createServerToServerDeploymentManager()
+          .createProjectDeployer(pmv)
+          .deploy(
+              "System", "localhost", new NullProgressMonitor(), new DeploymentLogger(Ivy.log()));
+      activate(pmv);
+    } catch (Exception ex) {
+      Ivy.log().error(ex);
+    } finally {
+      removeBackup(pmv.getProcessModel());
     }
   }
 
-  private static void sleep() {
+  private static void removeBackup(IProcessModel pm) {
+    DirectoryScanner scanner = new DirectoryScanner();
+    scanner.setBasedir(pm.getFileDirectory());
+    scanner.setIncludes(new String[] {"*Backup*.zip"});
+    scanner.scan();
+    for (String backup: scanner.getIncludedFiles()) {
+      try {
+        FileUtils.forceDelete(new File(scanner.getBasedir() + "/" + backup));
+        Ivy.log().info("Backup removed: {0}", backup);
+      } catch (IOException ex) {
+        Ivy.log().error("Could not remove backup after deployment.", ex);
+      }
+    }
+  }
+
+  private static void activate(IProcessModelVersion pmv) {
     try {
-      Thread.sleep(200);
-    } catch (InterruptedException ex) {
-      Ivy.log().error(ex);
+      pmv.activate();
+      Poll.await()
+          .withDelay(2, TimeUnit.SECONDS)
+          .untilOrTimeout(() -> pmv.getActivityOperationState() == ActivityOperationState.ACTIVE);
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
     }
   }
 
   private static void deactivate(IProcessModelVersion pmv) {
-    pmv.deactivate();
-    int timeOut = 10;
-    while (pmv.getActivityOperationState() != ActivityOperationState.INACTIVE) {
-      if (timeOut < 0) {
-        throw new RuntimeException("Process Model Version takes to long to deactivate.");
-      }
-      sleep();
-      timeOut -= 1;
+    try {
+      pmv.deactivate();
+      Poll.await()
+          .withDelay(2, TimeUnit.SECONDS)
+          .untilOrTimeout(() -> pmv.getActivityOperationState() == ActivityOperationState.INACTIVE);
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
     }
   }
 }
